0e770337ede66b19c18c6aa06d5b9963d3e91773
imujtabasidd-star <imujtabasidd@gmail.com>
2026-01-19 19:50:25 +0500

    Add DiscountedAmount class to calculate discounts

---
+++ Week-1/DiscountedAmount.java
@@ -0,0 +1,11 @@
+public class DiscountedAmount {
+public static void main (String[] args)
+{
+float csp=5000;
+float dp=10;
+float da=csp/dp;
+System.out.println("da: Rs" + da);
+float dsp=csp-da;
+System.out.println("dsp: Rs" + dsp);
+     }
+}
