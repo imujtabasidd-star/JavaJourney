3c3314321d0af368de17732932bdd96b1a1841b3
imujtabasidd-star <imujtabasidd@gmail.com>
2026-02-01 00:41:30 +0500

    Add CarpetCostCalculator with discount feature

--- Week-2/CarpetCostCalculator.java
+++ Week-2/CarpetCostCalculator.java
@@ -1,1 +1,17 @@
-
+public class CarpetCostCalculator {
+public static void main (String[] args)
+{
+double li=120;
+double bi=240;
+double lf=li/12;
+double bf=bi/12;
+double asf=lf*bf;
+double psf=200;
+double csp=asf*psf;
+System.out.println("Cost before discount: Rs" + csp);
+double d=10;
+double da=csp/d;
+double dsp=csp-da;
+System.out.println("Cost after discount: Rs" + dsp);
+     }
+}
