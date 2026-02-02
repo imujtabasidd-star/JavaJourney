ac9017a29893f694c2b617c39d65eff6711f564c
imujtabasidd-star <imujtabasidd@gmail.com>
2026-02-02 19:40:43 +0500

    Refactor CarpetCostCalculator to use user input

--- Week-2/CarpetCostCalculator.java
+++ Week-2/CarpetCostCalculator.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class CarpetCostCalculator {
-public static void main (String[] args)
-{
-double li=120;
-double bi=240;
-double lf=li/12;
-double bf=bi/12;
-double asf=lf*bf;
-double psf=200;
-double csp=asf*psf;
-System.out.println("Cost before discount: Rs" + csp);
-double d=10;
-double da=csp/d;
-double dsp=csp-da;
-System.out.println("Cost after discount: Rs" + dsp);
-     }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length (in inches): ");
+        double li = sc.nextDouble();
+
+        System.out.print("Enter breadth (in inches): ");
+        double bi = sc.nextDouble();
+
+        System.out.print("Enter price per sq ft: ");
+        double psf = sc.nextDouble();
+
+        System.out.print("Enter discount (%): ");
+        double d = sc.nextDouble();
+
+        double lf = li / 12;
+        double bf = bi / 12;
+
+        double asf = lf * bf;
+        double csp = asf * psf;
+
+        System.out.println("Cost before discount: Rs " + csp);
+
+        double da = (csp * d) / 100;
+        double dsp = csp - da;
+
+        System.out.println("Cost after discount: Rs " + dsp);
+
+        sc.close();
+    }
 }
